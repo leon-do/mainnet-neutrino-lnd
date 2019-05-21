54b68676b0e9325bb61727dc37691f4235aeb0f4
leon-do <leondo123@gmail.com>
2019-05-21 14:18:25 -0600

    mv perm

--- install.sh
+++ install.sh
@@ -5,13 +5,13 @@ sudo apt-get update
 echo "upgrading"
 sudo apt-get upgrade
 
+echo "installing npm"
+sudo apt install npm
+
 echo "update permissions"
 sudo chown -R $USER ~/.npm
 sudo chown -R $USER /usr/lib
 sudo chown -R $USER /usr/local
-
-echo "installing npm"
-sudo apt install npm
 
 echo "installing node-gyp"
 npm install -g node-gyp
