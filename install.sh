40e26393f48d8d80c6938183178cfa40f205fa25
leon-do <leondo123@gmail.com>
2019-05-21 13:53:35 -0600

    update perm

--- install.sh
+++ install.sh
@@ -5,17 +5,22 @@ sudo apt-get update
 echo "upgrading"
 sudo apt-get upgrade
 
+echo "update permissions"
+sudo chown -R $USER ~/.npm
+sudo chown -R $USER /usr/lib/node_modules
+sudo chown -R $USER /usr/local/lib/node_modules
+
 echo "installing npm"
-sudo apt install npm
+sudo apt-get install npm
+
+echo "installing node-gyp"
+npm install -g node-gyp
 
 echo "installing node"
 npm install -g n
 
 echo "updating node to lts"
 sudo n lts
-
-echo "installing node-gyp"
-npm install -g node-gyp
 
 echo "installing pm2"
 npm install -g pm2
